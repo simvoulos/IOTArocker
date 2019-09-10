62ad6ab10f320c7ff52410a22ecce5569230efb7
Symvoulakis Manolis <54994480+simvoulos@users.noreply.github.com>
2019-09-10 22:11:10 +0300

    Update CreateRockerAddress.js

--- CreateRockerAddress.js
+++ CreateRockerAddress.js
@@ -10,28 +10,26 @@ const iota = iotaLibrary.composeAPI({
   provider: 'https://nodes.thetangle.org:443'
 })
 
-const seed =
-  'YOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREY'
+const seed = 'YOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREYOURSEEDGOESHEREY'
+
 console.log('Creating Rocker\'s Security Level 1 addresses...\n')
 
-randomindex = Math.floor(Math.random() * 100000) + 1
+// Creating Listening addess
 iota
-  .getNewAddress(seed, { index: 0, total: 1, security: 1, checksum: true})
-  .then(address => {
+    .getNewAddress(seed, { index: 0, total: 1, security: 1, checksum: true})
+    .then(address => {
+        console.log('Rocker\'s Listening address: ' + address[0])
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-    console.log('Rocker\'s Listening address: ' + address[0])
-  })
-  .catch(err => {
-    console.log(err)
-  })
-
-  iota
-  .getNewAddress(seed, { index: randomindex, total: 1, security: 1, checksum: true})
-  .then(address => {
-
-    console.log('Rocker\'s Funding address  : ' + address[0])
-    console.log('!!! WARNING !!! \nIF THE FUNDING ADDRESS HAS BEEN USED BEFORE, RUN THIS TOOL AGAIN TO GENERATE A NEW ADDRESS')
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    // Creating funding address, which is he first unused address of the seed. 
+iota
+    .getNewAddress(seed, { security: 1, checksum: true, returnAll:true})
+    .then(address => {
+        console.log('Rocker\'s Funding address  : ' + address[address.length-1])
+    })
+    .catch(err => {
+        console.log(err)
+    })
